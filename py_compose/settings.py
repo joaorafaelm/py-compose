df2ddca0e63ad3ea87522e6030d6de9608034e19
João Rafael <joaoraf@me.com>
2017-10-08 17:47:59 -0300

    add settings file

---
+++ py_compose/settings.py
@@ -0,0 +1,14 @@
+from json import load as json_loader
+from yaml import load as yaml_loader
+
+CONFIG_FILENAME = 'py-compose'
+SUPORTED_EXTENSIONS = ('yaml', 'yml', 'json')
+
+PARSERS = {
+    # YAML
+    'yaml': yaml_loader,
+    'yml': yaml_loader,
+
+    # JSON
+    'json': json_loader
+}
