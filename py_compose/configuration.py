111a2f7ebe295f18f00d403087fd1a0dd0377dcd
Luiz F. A. de Prá <rawarkanis@gmail.com>
2017-10-22 16:37:31 -0200

    Add nargs suport for service_names

--- py_compose/configuration.py
+++ py_compose/configuration.py
@@ -1,52 +1,12 @@
-from os.path import isfile
+from os import chdir, environ
+from os.path import isfile, dirname
+from subprocess import Popen
+from collections import UserDict
+
 from py_compose.settings import (
     CONFIG_FILENAME, SUPORTED_EXTENSIONS, PARSERS,
     SERVICE_ATTRIBUTES
 )
-from subprocess import call, Popen
-from os import (chdir, environ)
-from os.path import dirname
-
-
-class Configuration:
-    filename = None
-    extension = None
-    services = {}
-
-    def __init__(
-            self, config_file=CONFIG_FILENAME, extensions=SUPORTED_EXTENSIONS
-    ):
-        if self.exists(config_file, extensions):
-            self.config = self._parse()
-            self._cast_services()
-
-            # If a custom file is specified, changes the working dir.
-            exec_dir = dirname(config_file)
-            if exec_dir:
-                chdir(exec_dir)
-
-    def _cast_services(self):
-        services = self.config.get('services')
-        for name, attributes in services.items():
-            filtered_attributes = {
-                key: attributes.get(key) for key in SERVICE_ATTRIBUTES
-            }
-            self.services.update({
-                name: Service(name, self, **filtered_attributes)
-            })
-
-    def _parse(self):
-        return PARSERS.get(self.extension)(open(self.filename, 'r'))
-
-    def exists(self, filename, extensions):
-        for extension in extensions:
-            if extension not in filename:
-                filename = '{}.{}'.format(filename, extension)
-            if isfile(filename):
-                self.filename = filename
-                self.extension = extension
-                return True
-        return False
 
 
 class Service:
@@ -98,3 +58,56 @@ class Service:
         if getattr(self, stage):
             command = ' && '.join(getattr(self, stage))
             self.subprocess = Popen(command, **params)
+
+
+class ServiceDict(UserDict):
+
+    def filter(self, service_names):
+        result = {}
+        for name in service_names:
+            service = self.data.get(name)
+            if service:
+                result[name] = service
+
+        return result
+
+
+class Configuration:
+    filename = None
+    extension = None
+    services = ServiceDict()
+
+    def __init__(
+            self, config_file=CONFIG_FILENAME, extensions=SUPORTED_EXTENSIONS
+    ):
+        if self.exists(config_file, extensions):
+            self.config = self._parse()
+            self._cast_services()
+
+            # If a custom file is specified, changes the working dir.
+            exec_dir = dirname(config_file)
+            if exec_dir:
+                chdir(exec_dir)
+
+    def _cast_services(self):
+        services = self.config.get('services')
+        for name, attributes in services.items():
+            filtered_attributes = {
+                key: attributes.get(key) for key in SERVICE_ATTRIBUTES
+            }
+            self.services.update({
+                name: Service(name, self, **filtered_attributes)
+            })
+
+    def _parse(self):
+        return PARSERS.get(self.extension)(open(self.filename, 'r'))
+
+    def exists(self, filename, extensions):
+        for extension in extensions:
+            if extension not in filename:
+                filename = '{}.{}'.format(filename, extension)
+            if isfile(filename):
+                self.filename = filename
+                self.extension = extension
+                return True
+        return False
